6f09f9c76d41807fb51979c8e2b412f547101f47
Infinical <inficalvin@gmail.com>
2021-06-03 09:48:22 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.infilabs.mfactor',
+  appName: 'mfactor-test',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
