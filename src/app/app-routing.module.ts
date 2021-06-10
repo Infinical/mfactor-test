f52b31ebd69546948682f0a2d3f19b7fe6d480a3
Calvin Mokua <inficalvin@gmail.com>
2021-06-10 07:43:35 +0300

    Add Home guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './helpers/auth.guard';
+import { HomeGuard } from './helpers/home.guard';
 
 const routes: Routes = [
   {
@@ -19,7 +20,7 @@ const routes: Routes = [
     path: 'auth',
     loadChildren: () =>
       import('./auth/auth.module').then((m) => m.AuthPageModule),
-    canActivate: [AuthGuard],
+    canActivate: [HomeGuard],
   },
 ];
 
